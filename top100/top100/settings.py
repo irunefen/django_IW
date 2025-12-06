0f984b627395c4bcb1a90be802789337115b67c2
irunefen <irunefen.fernandez@opendeusto.es>
2025-12-06 18:18:03 +0100

    Add settings for option 8

--- top100/top100/settings.py
+++ top100/top100/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i9j-ghmo=7ca)@$zumahd6kg2k0ida2nrk$da(3(+)0_daj&0n'
+#SECRET_KEY = 'django-insecure-i9j-ghmo=7ca)@$zumahd6kg2k0ida2nrk$da(3(+)0_daj&0n'
+# Clave de prueba
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "dev-secret-key"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS con el dominio
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 
 # Application definition
@@ -116,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
